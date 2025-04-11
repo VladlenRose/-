31f4b7b7a8b3b290ef30e79f8c2be07239740f8d
VladlenRose <115209995+VladlenRose@users.noreply.github.com>
2025-04-11 23:03:40 +0400

    Create Vertex.cs

---
+++ Vertex.cs
@@ -0,0 +1,12 @@
+using System.Drawing;
+
+namespace GraphGame
+{
+    public class Vertex
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Weight { get; set; }
+        public Color VertexColor { get; set; }
+    }
+}
